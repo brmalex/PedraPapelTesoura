1defd01799cbb74df89b337512f1b1e48e429983
Bruno Martins Alexandre <brunomartinsalexandre@hotmail.com>
2022-10-05 11:18:39 -0300

    versao 2
    Pedra, papel, tesoura, lagarto, Spock

--- pedrapapeltesoura.c
+++ pedrapapeltesoura.c
@@ -7,9 +7,9 @@ int main(){
 	srand(time(NULL));
 	
 	do{
-		printf("Escolha sua jogada:\n1)Pedra\n2)Papel\n3)Tesoura\n4)Sair\n: ");
+		printf("Escolha sua jogada:\n1)Pedra\n2)Papel\n3)Tesoura\n4)Lagarto\n5)Spock\n6)Sair\n: ");
 		scanf("%d",&player);
-		cpu = rand() % 3 + 1;
+		cpu = rand() % 5 + 1;
 		switch(player){
 			case 1:
 				switch(cpu){
@@ -21,6 +21,12 @@ int main(){
 						break;
 					case 3:
 						printf("Jogador: pedra\nCPU: tesoura\nVitoria\n\n");
+						break;
+					case 4:
+						printf("Jogador: pedra\nCPU: lagarto\nVitoria\n\n");
+						break;
+					case 5:
+						printf("Jogador: pedra\nCPU: Spock\nDerrota\n\n");
 						break;
 				}
 				break;
@@ -35,6 +41,12 @@ int main(){
 					case 3:
 						printf("Jogador: papel\nCPU: tesoura\nDerrota\n\n");
 						break;
+					case 4:
+						printf("Jogador: papel\nCPU: lagarto\nDerrota\n\n");
+						break;
+					case 5:
+						printf("Jogador: papel\nCPU: Spock\nVitoria\n\n");
+						break;
 				}
 				break;
 			case 3:
@@ -48,9 +60,53 @@ int main(){
 					case 3:
 						printf("Jogador: tesoura\nCPU: tesoura\nEmpate\n\n");
 						break;
+					case 4:
+						printf("Jogador: tesoura\nCPU: lagarto\nVitoria\n\n");
+						break;
+					case 5:
+						printf("Jogador: tesoura\nCPU: Spock\nDerrota\n\n");
+						break;
 				}
 				break;
 			case 4:
+				switch(cpu){
+					case 1:
+						printf("Jogador: lagarto\nCPU: pedra\nDerrota\n\n");
+						break;
+					case 2:
+						printf("Jogador: lagarto\nCPU: papel\nVitoria\n\n");
+						break;
+					case 3:
+						printf("Jogador: lagarto\nCPU: tesoura\nDerrota\n\n");
+						break;
+					case 4:
+						printf("Jogador: lagarto\nCPU: lagarto\nEmpate\n\n");
+						break;
+					case 5:
+						printf("Jogador: lagarto\nCPU: Spock\nVitoria\n\n");
+						break;
+				}
+				break;
+			case 5:
+				switch(cpu){
+					case 1:
+						printf("Jogador: Spock\nCPU: pedra\nVitoria\n\n");
+						break;
+					case 2:
+						printf("Jogador: Spock\nCPU: papel\nDerrota\n\n");
+						break;
+					case 3:
+						printf("Jogador: Spock\nCPU: tesoura\nVitoria\n\n");
+						break;
+					case 4:
+						printf("Jogador: Spock\nCPU: lagarto\nDerrota\n\n");
+						break;
+					case 5:
+						printf("Jogador: Spock\nCPU: Spock\nEmpate\n\n");
+						break;
+				}
+				break;
+			case 6:
 				game = -1;
 				printf("Saindo...");
 				break;
@@ -60,4 +116,3 @@ int main(){
 		}
 	}while(game != -1);
 }
-
